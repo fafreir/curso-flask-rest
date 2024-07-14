72219a7b50cd4996de6ba8dde77315dc95170e95
Fabrício Adriel <fabricioadriel@yahoo.com.br>
2024-07-14 15:14:21 -0300

    Atualizado GET, POST e PUT

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 hoteis = [
     {
@@ -31,17 +31,50 @@ class Hoteis(Resource):
 
 
 class Hotel(Resource):
-    def get(self, hotel_id):
+    argumentos = reqparse.RequestParser()
+    # Limita para não colocar dados não solicitados
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas')
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
+
+    def find_hotel(hotel_id):
         for hotel in hoteis:
             if hotel['hotel_id'] == hotel_id:
                 return hotel
+        return None
+
+    def get(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
         return {'message': 'Hotel not found'}, 404
 
     def post(self, hotel_id):
-        pass
+        # Recebo no formato chave: valor
+        dados = Hotel.argumentos.parse_args()
+
+        novo_hotel = {
+            'hotel_id': hotel_id,
+            'nome': dados['nome'],
+            'estrelas': dados['estrelas'],
+            'diaria': dados['diaria'],
+            'cidade': dados['cidade']
+        }
+
+        hoteis.append(novo_hotel)
+        return novo_hotel, 200
 
     def put(self, hotel_id):
-        pass
+        dados = Hotel.argumentos.parse_args()
+        novo_hotel = {'hotel_id': hotel_id, **dados}
+
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)
+            return novo_hotel, 200  # OK
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201  # Created
 
     def delete(self, hotel_id):
         pass
