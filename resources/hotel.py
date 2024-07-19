81764774c3659dfa51b5c149a60b5599dfd69b78
Fabrício Adriel Coelho Freire <fabricioadriel@yahoo.com.br>
2024-07-19 06:41:03 -0300

    Inserindo required ao campo e mensagem de erro ao falhar o banco

--- resources/hotel.py
+++ resources/hotel.py
@@ -35,8 +35,8 @@ class Hoteis(Resource):
 class Hotel(Resource):
     argumentos = reqparse.RequestParser()
     # Limita para não colocar dados não solicitados
-    argumentos.add_argument('nome')
-    argumentos.add_argument('estrelas')
+    argumentos.add_argument('nome', type=str, required=True, help="The field 'nome' cannot be left blank!")
+    argumentos.add_argument('estrelas', type=float, required=True, help="The field 'estrelas' cannot be left blank!")
     argumentos.add_argument('diaria')
     argumentos.add_argument('cidade')
 
@@ -51,7 +51,10 @@ class Hotel(Resource):
             return {'message': 'Hotel id "{}" already exists'.format(hotel_id)}, 400
         dados = Hotel.argumentos.parse_args()
         hotel = HotelModel(hotel_id, **dados)
-        hotel.save_hotel()
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message':'An internal error ocurred trying to save'}, 500 # Internal Server Error
         return hotel.json()
 
     def put(self, hotel_id):
@@ -63,12 +66,18 @@ class Hotel(Resource):
             hotel_encontrado.save_hotel()
             return hotel_encontrado.json(), 200  # OK
         hotel = HotelModel(hotel_id, **dados)
-        hotel.save_hotel()
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message':'An internal error ocurred trying to save'}, 500 # Internal Server Error
         return hotel.json(), 201  # Created
 
     def delete(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            hotel.delete_hotel()
+            try:
+                hotel.delete_hotel()
+            except:
+                return {'message': 'An error ocurred trying to delete hotel.'}, 500
             return {'message': 'Hotel deleted.'}
         return {'message': 'Hotel not found.'}, 404
