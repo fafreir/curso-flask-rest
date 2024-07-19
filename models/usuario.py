d914a50981faec6322bcbc1a468ac63e95c65771
Fabrício Adriel Coelho Freire <fabricioadriel@yahoo.com.br>
2024-07-19 11:10:52 -0300

    Criacao de models de usuario

---
+++ models/usuario.py
@@ -0,0 +1,36 @@
+from sql_alchemy import banco 
+
+class UserModel(banco.Model):
+    __table__ = 'usuarios'
+
+    user_id = banco.Column(banco.Integer, primary_key = True)
+    login = banco.Column(banco.String(40))
+    senha = banco.Column(banco.String(40))
+
+    def __init__(self, login, senha):
+        self.login = login
+        self.senha = senha 
+    
+    def json(self):
+        return {
+            'user_id': self.user_id,
+            'login': self.login
+        }
+    
+    @classmethod
+    def find_user(cls, user_id):
+        user = cls.query.filter_by(user_id = user_id).first() 
+        if user:
+            return user 
+        return None 
+    
+    # Cadastrar usuario
+    def save_user(self):
+        banco.session.add(self)
+        banco.session.commit()
+
+    def delete_hotel(self):
+        banco.session.delete(self)
+        banco.session.commit()
+    
+    
