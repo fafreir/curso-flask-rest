48435a73c371c49b0348b8f55294b83c315cf2e5
Fabrício Adriel <fabricioadriel@yahoo.com.br>
2024-07-20 18:52:14 -0300

    Atualizado Resource de usuario

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,5 +1,13 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
+from flask_jwt_extended import create_access_token
+from werkzeug.security import safe_str_cmp
+
+atributos = reqparse.RequestParser()
+atributos.add_argument('login', type=str, required=True,
+                       help="The field 'login' cannot left be")
+atributos.add_argument('senha', type=str, required=True,
+                       help="The field 'senha' cannot left be")
 
 
 class User(Resource):
@@ -23,11 +31,7 @@ class User(Resource):
 class UserRegister(Resource):
     # /cadastro
     def post(self):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument('login', type=str, required=True,
-                               help="The field 'login' cannot left be")
-        atributos.add_argument('senha', type=str, required=True,
-                               help="The field 'senha' cannot left be")
+
         dados = atributos.parse_args()
 
         if UserModel.find_by_login(dados['login']):
@@ -36,4 +40,17 @@ class UserRegister(Resource):
 
         user = UserModel(**dados)
         user.save_user()
-        return {'message': 'User create sucessfully'}, 201
+        return {'message': 'User created sucessfully'}, 201
+
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+        user = UserModel.find_by_login(dados['login'])
+
+        if user and safe_str_cmp(user.senha, dados['senha']):
+            token_de_acesso = create_access_token(identity=user.user_id)
+            return {'acess_token': token_de_acesso}, 200
+        return {'message': 'The username or password is incorrect'}, 401
