b1a65f8e1ac48b71e95ef3f55a4308b022a3618e
Fabrício Adriel <fabricioadriel@yahoo.com.br>
2024-07-20 00:35:54 -0300

    Finalizando usuarios

--- models/usuario.py
+++ models/usuario.py
@@ -1,29 +1,37 @@
-from sql_alchemy import banco 
+from sql_alchemy import banco
+
 
 class UserModel(banco.Model):
     __table__ = 'usuarios'
 
-    user_id = banco.Column(banco.Integer, primary_key = True)
+    user_id = banco.Column(banco.Integer, primary_key=True)
     login = banco.Column(banco.String(40))
     senha = banco.Column(banco.String(40))
 
     def __init__(self, login, senha):
         self.login = login
-        self.senha = senha 
-    
+        self.senha = senha
+
     def json(self):
         return {
             'user_id': self.user_id,
             'login': self.login
         }
-    
+
     @classmethod
     def find_user(cls, user_id):
-        user = cls.query.filter_by(user_id = user_id).first() 
+        user = cls.query.filter_by(user_id=user_id).first()
         if user:
-            return user 
-        return None 
-    
+            return user
+        return None
+
+    @classmethod
+    def find_by_login(cls, login):
+        user = cls.query.filter_by(login=login).first()
+        if user:
+            return user
+        return None
+
     # Cadastrar usuario
     def save_user(self):
         banco.session.add(self)
@@ -32,5 +40,3 @@ class UserModel(banco.Model):
     def delete_user(self):
         banco.session.delete(self)
         banco.session.commit()
-    
-    
--- resources/usuario.py
+++ resources/usuario.py
@@ -1,5 +1,6 @@
-from flask_sqlalchemy import Resource, reqparse 
+from flask_sqlalchemy import Resource, reqparse
 from models.usuario import UserModel
+
 
 class User(Resource):
 
@@ -8,12 +9,30 @@ class User(Resource):
         user = UserModel.find_user(user_id)
         if user:
             return user.json()
-        return {'message':'User not found.'}, 404
-    
+        return {'message': 'User not found.'}, 404
+
     # deletar
     def delete(self, user_id):
         user = UserModel.find_user(user_id)
         if user:
             user.delete_user()
-            return {'message':'User deleted.'}
-        return {'message':'User not found.'}
+            return {'message': 'User deleted.'}
+        return {'message': 'User not found.'}
+
+
+class UserRegister(Resource):
+    # /cadastro
+    def post(self):
+        atributos = reqparse.RequestParser()
+        atributos.add_argument('login', type=str, required=True,
+                               help="The field 'login' cannot left be")
+        atributos.add_argument('senha', type=str, required=True,
+                               help="The field 'senha' cannot left be")
+        dados = atributos.parse_args()
+
+        if UserModel.find_by_login(dados['login']):
+            return {"message": "The login '{}' already exists.".format{dados['login']}}
+
+        user = UserModel(**dados)
+        user.save_user()
+        return {'message': 'User create sucessfully'}, 201
